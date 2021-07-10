d208129a7b0847b1ff7c86b0457027f949db7f6b
saurabh <saurabhdantani09@gmail.com>
2021-07-10 14:30:29 +0530

    hello

--- app1/views.py
+++ app1/views.py
@@ -1,11 +1,8 @@
-from django.http.response import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpRequest
+#Create your views here.
 
 from django.http import HttpResponse
 
 def home(request):
-    return HttpRequest("hello world")
-    
+	return HttpResponse("hello world")
--- heroku/urls.py
+++ heroku/urls.py
@@ -18,5 +18,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app1/', include('heroku.app1')),
+    path('', include('app1.heroku_url')),
 ]
