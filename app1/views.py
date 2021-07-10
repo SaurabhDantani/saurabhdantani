57236bd125cda0350042f772ec87768b8f5c5fd8
saurabh <saurabhdantani09@gmail.com>
2021-07-10 14:17:17 +0530

    update

--- app1/views.py
+++ app1/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpRequest
 
+from django.http import HttpResponse
+
 def home(request):
     return HttpRequest("hello world")
     
